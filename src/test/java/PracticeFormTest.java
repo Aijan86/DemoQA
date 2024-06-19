687b06940b4e3f0f76ee978871a02e3d8927e065
aijan <aijankydybaeva@gmail.com>
2024-06-19 14:30:50 +0600

    Test new

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -15,31 +15,33 @@ import java.util.Random;
 @Listeners(com.demoqa.listener.ScreenshotListener.class)
 public class PracticeFormTest extends BaseTest{
 
-    @BeforeClass(alwaysRun = true)
-    public void setUp() {
-        driver = DriverManager.getDriver();
-    }
+//    @BeforeClass(alwaysRun = true)
+//    public void setUp() {
+//        driver = DriverManager.getDriver();
+//    }
 
 
     @Test
     public void practiceFormTest() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
+        browserHelper.open("https://demoqa.com/automation-practice-form");
+//        driver.get("https://demoqa.com/automation-practice-form");
         PracticeFormEntity practiceFormEntity = randomUtils.generateRandomPracticeFormEntity();
         demoQAPages.getPracticeFormPage().fillUpPracticeForm(practiceFormEntity);
-        Thread.sleep(5000);
 
     }
 
     @Test
     public void test123() throws InterruptedException {
         browserHelper.open("https://demoqa.com/automation-practice-form");
-
         demoQAPages.getPracticeFormPage().selectDateMonthYear("30 June 2020");
-        Thread.sleep(5000);
     }
 
     // Local Date
     // Local Time
 
+        @AfterClass(alwaysRun = true)
+        public void tearDown()  {
+            DriverManager.closeDriver();
+        }
 
 }
