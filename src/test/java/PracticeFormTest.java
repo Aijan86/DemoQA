665d18e6738272a50cd258e6e55d0f9d2810a5eb
aijan <aijankydybaeva@gmail.com>
2024-06-20 14:11:37 +0600

    new

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -21,21 +21,20 @@ public class PracticeFormTest extends BaseTest{
 //    }
 
 
-//    @Test
-//    public void practiceFormTest() throws InterruptedException {
-//        browserHelper.open("https://demoqa.com/automation-practice-form");
-////        driver.get("https://demoqa.com/automation-practice-form");
-//        PracticeFormEntity practiceFormEntity = randomUtils.generateRandomPracticeFormEntity();
-//        demoQAPages.getPracticeFormPage().fillUpPracticeForm(practiceFormEntity);
-//
-//    }
+    @Test
+    public void practiceFormTest() throws InterruptedException {
+        browserHelper.open("https://demoqa.com/automation-practice-form");
+     driver.get("https://demoqa.com/automation-practice-form");
+        PracticeFormEntity practiceFormEntity = randomUtils.generateRandomPracticeFormEntity();
+        demoQAPages.getPracticeFormPage().fillUpPracticeForm(practiceFormEntity);
 
+    }
+
+    @Test
  public void test123() throws InterruptedException {
         browserHelper.open("https://demoqa.com/automation-practice-form");
         demoQAPages.getPracticeFormPage().selectDateMonthYear("30 June 2020");
     }
 
-    // Local Date
-    // Local Time
 
 }
